3d69db262b5713456f92a647def0027b1150b54d
Andrei Fokau <andrei.fokau@neutron.kth.se>
2011-11-19 12:41:30 +0100

    save reqs in file to be used by pip

--- pyhub/pyhub.py
+++ pyhub/pyhub.py
@@ -23,13 +23,14 @@ class Command():
         for line in lines:
             line = line.split('#')[0].strip()
             if len(line):
+                print ' --> processing: %s' % line
                 try:
                     m = re.search(r'(?P<user>\S+)/(?P<repo>\S+)(\s*@(?P<ref>\S+))?', line)
                     sources.append(GitHubRepo(user=m.group('user'), repo=m.group('repo'), ref=m.group('ref')))
                 except Exception as e:
                     sys.stderr.write('Error for req "%s": %s\n' % (line, e))
         reqs = '\n'.join([s.req() or s.line() for s in sources])
-        sources = '\n'.join([s.line() for s in sources])
+        sources = '\n'.join([s.get_unpack_path() for s in sources])
         return sources, reqs
 
 
@@ -42,7 +43,8 @@ def command():
     c = Command()
     sources, reqs = c.requirements('requirements.github')
     print sources
-    write_to_file('requirements.txt', reqs+'\n')
+    write_to_file('requirements.txt', sources+'\n')
+    write_to_file('requirements.pip', reqs+'\n')
 
 
 if __name__ == '__main__':
